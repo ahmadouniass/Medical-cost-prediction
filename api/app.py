76a2cd0af635d0e1d305df33c1d3b0d290e58b5a
ahmadouniass <ahmadouniass2@gmail.com>
2026-01-13 04:31:57 +0000

    adaptation de app et déplacement du modele

--- api/app.py
+++ api/app.py
@@ -16,17 +16,14 @@ BASE_DIR = Path(__file__).resolve().parent
 # =========================
 
 # Chemin du modèle (modifiable via variable d'environnement)
-# - Si la variable d'env MODEL_PATH existe -> on l'utilise
 # - Sinon -> valeur par défaut : "models/best_gradient_boost_model.joblib"
-DEFAULT_MODEL_PATH = (BASE_DIR.parent / "models" / "best_gradient_boost_model.joblib")
-MODEL_PATH = str(DEFAULT_MODEL_PATH)
+MODEL_PATH = "models/best_gradient_boost_model.joblib"
 
 # Clé API stockée côté serveur (variable d'environnement)
 # Si API_KEY n’est pas définie dans l’environnement -> API ouverte sans authentification
 API_KEY = os.getenv("API_KEY")  # ex: "mon-secret"
 
-print("[DEBUG] Using MODEL_PATH:", MODEL_PATH)
-print("[DEBUG] Exists?:", Path(MODEL_PATH).exists())
+
 
 # Création de l’application FastAPI (nom affiché dans /docs)
 app = FastAPI(title="Medical Cost Prediction API")
